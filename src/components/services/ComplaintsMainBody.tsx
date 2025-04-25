30c6ee1e2d03a0db08f5d3163c11c923e898d299
Sheshin-Reddy <sheshin.shivampeta@pracbee.com>
2025-04-25 17:21:56 +0530

    improved sidebar styling

---
+++ src/components/services/ComplaintsMainBody.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from "@mui/material";
+
+function ComplaintsMainBody() {
+    return (
+        <Stack>
+            <Typography variant="h4">Complaints</Typography>
+        </Stack>
+    )
+}
+
+export default ComplaintsMainBody;
